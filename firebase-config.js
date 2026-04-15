f8eb089a5545eb84700e6a7281fbf2c7ea7bec84
RifqyAja <156778697+rydevs29@users.noreply.github.com>
2026-04-15 14:32:19 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-analytics.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-database.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA1WHFjidCIEotEG4nOOUCV1OHU-cslddU",
+  authDomain: "rifqymetrics.firebaseapp.com",
+  projectId: "rifqymetrics",
+  storageBucket: "rifqymetrics.firebasestorage.app",
+  messagingSenderId: "459868583825",
+  appId: "1:459868583825:web:c2f532f800d584b8b14c30",
+  measurementId: "G-0HV0S6LRHB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Export layanan agar bisa dipakai di index.js dan admin.js
+export const db = getDatabase(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
